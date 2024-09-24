0ff2b0cb6fac74c489ceb9dbdc7f755b46f6f9a7
Rene Rodriguez <renerod1@hotmail.com>
2024-09-24 01:24:47 -0400

    Updated README

--- update_config.py
+++ update_config.py
@@ -5,14 +5,15 @@ import os
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-# Get the environment variable for the GitHub username
+# Get the environment variable
 github_user_name = 'renerod1'
-gif_frame_duration = '10000'
+gif_frame_duration = '7500'
+generate_merged_prs = true
 
-
-# Update the github_user_name in the config file
-config.set('Settings', 'github_user_name', github_user_name)
+# Override the variable in the config file
 config.set('Settings', 'gif_frame_duration', gif_frame_duration)
+config.set('Settings', 'github_user_name', github_user_name)
+config.set('Readme', 'generate_merged_prs', generate_merged_prs)
 
 # Write the changes back to the config.ini file
 with open('config.ini', 'w') as configfile:
