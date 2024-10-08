37f2886d13e2eb42a71d70df29ac74efe0ac0eb2
Rene Rodriguez <renerod1@hotmail.com>
2024-10-08 07:55:19 -0400

    Updated configuration

--- update_config.py
+++ update_config.py
@@ -3,18 +3,18 @@ import os
 
 # Load the config.ini file
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read("config.ini")
 
 # Get the environment variable
-github_user_name = 'renerod1'
-gif_frame_duration = '7500'
-generate_merged_prs = 'True'
+github_user_name = "renerod1"
+gif_frame_duration = "7500"
+generate_merged_prs = "true"
 
 # Override the variable in the config file
-config.set('Settings', 'gif_frame_duration', gif_frame_duration)
-config.set('Settings', 'github_user_name', github_user_name)
-config.set('Readme', 'generate_merged_prs', generate_merged_prs)
+config.set("Settings", "gif_frame_duration", gif_frame_duration)
+config.set("Settings", "github_user_name", github_user_name)
+config.set("Readme", "generate_merged_prs", generate_merged_prs)
 
 # Write the changes back to the config.ini file
-with open('config.ini', 'w') as configfile:
+with open("config.ini", "w") as configfile:
     config.write(configfile)
